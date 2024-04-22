338c947d341121e215bfeef9d59fdc8c16a137a6
Jheison Martinez <jmartinez@vettica.co>
2024-04-22 10:59:48 -0500

    Se agrega conexión con base de datos

---
+++ db.js
@@ -0,0 +1,8 @@
+import mysql from 'mysql'
+
+mysql.createConnection({
+    host: "database-app-test.cf4swwgeaq99.us-east-1.rds.amazonaws.com",
+    database: "appGestionActifividades",
+    user: "admin",
+    password: "Marbol26"
+})
