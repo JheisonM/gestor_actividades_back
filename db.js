109f9fe0cb8ccbde98f7dd06d7b393bb489c4044
Jheison Martinez <jmartinez@vettica.co>
2024-04-22 12:33:23 -0500

    Cambios es db.js

--- db.js
+++ db.js
@@ -1,8 +1,18 @@
 import mysql from 'mysql'
 
-mysql.createConnection({
+let conexion = mysql.createConnection({
     host: "database-app-test.cf4swwgeaq99.us-east-1.rds.amazonaws.com",
     database: "appGestionActifividades",
     user: "admin",
     password: "Marbol26"
 })
+
+conexion.connect((err) => {
+    if (err) {
+      console.error('Error al conectar a la base de datos:', err);
+      return;
+    }
+    console.log('Conexión establecida correctamente con la base de datos');
+  });
+  
+  export default conexion;
